acf98775f34a68f720839146c3593dd12a2d2ce0
Sadaqathocein <sadaqathocein72@gmail.com>
2025-02-17 14:15:58 +0500

    Sadaqat

---
+++ GradeTask1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+ 
+ public class GradeTask1{
+     
+    public static void main(String [] args){ 
+
+    int num1, num2, num3, average;
+
+    num1 = 125;
+  
+    num2 = 28;
+
+    num3 = -25;
+
+   average = (num1 + num2 + num3)/3;
+
+   System.out.print("num1 = " + num1);
+
+   System.out.print("num2 = " + num2);
+
+   System.out.print("num3 = " + num3);
+
+   System.out.print("average = " + average);
+
+  }
+
+}
+
+   
+   
